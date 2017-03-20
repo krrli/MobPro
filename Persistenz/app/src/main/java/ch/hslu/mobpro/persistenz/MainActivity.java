a190a57eb15ff31ff07936ff844f7e9bd2f7e1d7
krrli <carlaiten@gmail.com>
2017-03-20 19:50:06 +0100

    added permission part, added save external / internal functionality

--- Persistenz/app/src/main/java/ch/hslu/mobpro/persistenz/MainActivity.java
+++ Persistenz/app/src/main/java/ch/hslu/mobpro/persistenz/MainActivity.java
@@ -1,17 +1,40 @@
 package ch.hslu.mobpro.persistenz;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
+import android.os.Environment;
 import android.preference.PreferenceManager;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends Activity {
 
     private int resumeCount;
@@ -20,6 +43,13 @@ public class MainActivity extends Activity {
     private TextView txtTeaPrefsSummary;
     private Button btnTeaPrefEdit;
     private Button btnSetDefaultPreferences;
+    private CheckBox cbUseExternalStorage;
+    private Button btnSave;
+    private Button btnLoad;
+    private TextView lblTextFileContent;
+    private TextView lblExtStorage;
+    private final String filename = "InternalFile";
+    private EditText editedTxtFileText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +60,12 @@ public class MainActivity extends Activity {
         txtTeaPrefsSummary = (TextView) findViewById(R.id.txtTeaPrefSummary);
         btnTeaPrefEdit = (Button) findViewById(R.id.btnEditPreferences);
         btnSetDefaultPreferences = (Button) findViewById(R.id.btnSetPreferences);
+        cbUseExternalStorage = (CheckBox) findViewById(R.id.cbExternerSpeicher);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnLoad = (Button) findViewById(R.id.btnLoad);
+        lblTextFileContent = (TextView) findViewById(R.id.lblTextFileContent);
+        editedTxtFileText = (EditText) findViewById(R.id.editedTxtFileText);
+        lblExtStorage = (TextView) findViewById(R.id.lblExtStorage);
 
         sharedPreferences = getPreferences(MODE_PRIVATE);
         resumeCount = sharedPreferences.getInt("CounterAppPref",0);
@@ -45,29 +81,134 @@ public class MainActivity extends Activity {
         btnSetDefaultPreferences.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-
-                // fixe Werte in Preferences speichern
-                // Gesüsster Tee, Süssstoff honig und Marke/Sorte ginger
-
-                final SharedPreferences.Editor teaPrefEditor = PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit();
-
-                String teaPref = "Ginger from se root";
-                String sweetenerPref = "natural";
-
-                teaPrefEditor.putString("teaPreferred", teaPref);
-                teaPrefEditor.putString("teaSweetener", sweetenerPref);
-                teaPrefEditor.putBoolean("teaWithSugar", true);
-                teaPrefEditor.apply();
-
-                String sweetenerPrefText = translateSweetenerValuesIntoText(sweetenerPref);
-
-                String textSummaryPt = String.format(getResources().getString(R.string.teaPrefsSummary), teaPref);
-                textSummaryPt =  textSummaryPt + String.format(getResources().getString(R.string.teaPrefsWithSweetenerSummary), sweetenerPrefText);
-                txtTeaPrefsSummary.setText(textSummaryPt);
+                defaultPreferences();
             }
         });
 
+        if (isExternalStorageWriteable())
+            lblExtStorage.setText("External storage is mounted (writable)");
+        else if (isExternalStorageReadable())
+            lblExtStorage.setText("External storage is mounted (readable)");
+        else
+            lblExtStorage.setText("External storage is not mounted");
+        
+
+        btnSave.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                saveFile();
+            }
+        });
+
+        btnLoad.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                readFile();
+            }
+        });
+
+    }
+
+    private void defaultPreferences(){
+        PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        final SharedPreferences.Editor teaPrefEditor = PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit();
+
+        String teaPref = "Ginger from se root";
+        String sweetenerPref = "natural";
+
+        teaPrefEditor.putString("teaPreferred", teaPref);
+        teaPrefEditor.putString("teaSweetener", sweetenerPref);
+        teaPrefEditor.putBoolean("teaWithSugar", true);
+        teaPrefEditor.apply();
+
+        String sweetenerPrefText = translateSweetenerValuesIntoText(sweetenerPref);
+
+        String textSummaryPt = String.format(getResources().getString(R.string.teaPrefsSummary), teaPref);
+        textSummaryPt =  textSummaryPt + String.format(getResources().getString(R.string.teaPrefsWithSweetenerSummary), sweetenerPrefText);
+        txtTeaPrefsSummary.setText(textSummaryPt);
+    }
+    private void saveFile(){
+        if (cbUseExternalStorage.isChecked() && isExternalStorageWriteable()) {
+            MainActivityPermissionsDispatcher.writeFileToExternalStorageWithCheck(MainActivity.this, editedTxtFileText.getText().toString());
+        } else {
+            writeFileToInternalStorage(editedTxtFileText.getText().toString());
+        }
+        editedTxtFileText.setText("");
+    }
+
+    private void readFile() {
+        if (cbUseExternalStorage.isChecked()) {
+           MainActivityPermissionsDispatcher.readFileToExternalStorageWithCheck(this);
+        } else {
+            lblTextFileContent.setText(readInternalFile());
+        }
+    }
+
+    private String readInternalFile() {
+        FileInputStream fis = null;
+        try {
+            fis = getApplicationContext().openFileInput(filename);
+
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isExternalStorageWriteable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    @NeedsPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void writeFileToExternalStorage(String content) {
+        File root = android.os.Environment.getExternalStorageDirectory();
+        File dir = new File(root.getAbsolutePath() + "/Persistenz");
+        dir.mkdirs();
+        File file = new File(dir, filename);
+
+        try {
+            FileOutputStream f = new FileOutputStream(file);
+            f.write(content.getBytes());
+            f.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeFileToInternalStorage(String text) {
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(text.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private String translateSweetenerValuesIntoText(String value){
@@ -87,12 +228,6 @@ public class MainActivity extends Activity {
 
         return value;
 
-    }
-
-
-    private void readTeaPreferences(){
-
-        txtTeaPrefsSummary.setText("hoi");
     }
 
     @Override
@@ -131,4 +266,98 @@ public class MainActivity extends Activity {
 
         txtTeaPrefsSummary.setText(textSummaryPt);
     }
+
+
+
+    @OnShowRationale(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showRationalforExternalStorage(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage("Die App benötigt zugriff auf den Externen Speicher, bitte säg jo!")
+                .setPositiveButton("Erlauben", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .show();
+    }
+
+
+    @OnPermissionDenied(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showDeniedForCamera() {
+        Toast.makeText(this, "wiso seisch nei????", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showNeverAskForCamera() {
+        Toast.makeText(this, "dont show camera plis", Toast.LENGTH_SHORT).show();
+    }
+
+    @NeedsPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    void readFileToExternalStorage() {
+        File root = android.os.Environment.getExternalStorageDirectory();
+        File dir = new File(root.getAbsolutePath() + "/Persistenz");
+        dir.mkdirs();
+        File file = new File(dir, filename);
+
+        String myData = "";
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                myData = myData + strLine;
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        lblTextFileContent.setText(myData);
+    }
+
+    @OnShowRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showRationalforReadExternalStorage(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage("Die App benötigt zugriff auf den Externen Speicher, bitte säg jo!")
+                .setPositiveButton("Erlauben", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .show();
+    }
+
+
+    @OnPermissionDenied(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showDeniedForReadExtStorage() {
+        Toast.makeText(this, "nei nei, wetti ned!", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showNeverAskForReadExtStorage() {
+        Toast.makeText(this, "wiso? besch secher? neiiiii", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
 }

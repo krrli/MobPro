5bc9f3c1ea90932b2481baeef0365bb7312f511b
krrli <carlaiten@gmail.com>
2017-05-01 19:51:00 +0200

    few changes to complete exercise - layout of index.html and movie.js

--- MovieBrowser/js/movies.js
+++ MovieBrowser/js/movies.js
@@ -5,21 +5,34 @@ function showMovies(){
         url: 'http://www.omdbapi.com/?y=&plot=short&r=json&t=' + movieToFind,
         dataType: 'jsonp',
         jsonp: 'callback',
-        jsonpCallback: 'machOepis',
+        jsonpCallback: 'foellDasAb',
         error: function () {
-            alert("there's something wrong...");
+            alert("nüt gfonde...");
         }
     });
 
     //alert('Result: '+JSON.stringify(result))
 }
 
-function machOepis(data){
+function foellDasAb(data){
     if (data.Response == "False") {
-        alert("there's something wrong...");
+        alert("nüt gfonde...");
         return;
     }
 
-    alert(data.Title + " "  + data.Year);
+    $("#txtTitle").val();
+    $("#txtYear").val();
+    $("#txtActors").empty();
+    $("#txtPlot").empty();
+    $("#txtRating").val();
 
+
+    $("#txtTitle").val(data.Title);
+    $("#txtYear").val(data.Year);
+    $("#txtActors").val(data.Actors);
+    $("#txtPlot").val(data.Plot);
+    $("#txtRating").val(data.Ratings[0].Value);
+    $("#imgPoster").attr("src", data.Poster);
+
+    $("body").pagecontainer("change", "#page-movie", { transition: "flip" });
 }
